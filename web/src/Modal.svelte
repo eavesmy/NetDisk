a4b08c9944123c6f20417c596f27952ac468690a
eavesmy <eavesmy@gmail.com>
2020-01-16 00:10:58 +0800

    web

---
+++ web/src/Modal.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let FileName;
+	export let Type;
+	export let Component;
+	export let FilePath;
+	
+	function closeModal(){
+		const $this = this;
+		document.querySelectorAll(".modal").forEach(it => {
+			it.className = "modal";
+		});
+	}
+</script>
+
+<div>
+	<div id="modal" class="modal">
+		<div class="modal-background" on:click={closeModal}></div>
+		<div class="modal-content">
+			<div class="card box">
+				<header class="card-header subtitle is-5">
+					{FileName}
+				</header>
+				<div class="card-content">
+					<Component {FilePath}/>
+				</div>
+			</div>
+		</div>
+		<button class="modal-close is-large" aria-label="close" on:click={closeModal}></button>
+	</div>
+</div>
+
+<style> </style>
